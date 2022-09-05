aeeec5aa862f905a313d34bd6a2e66da0246da7a
hidekiprado <104890292+hidekiprado@users.noreply.github.com>
2022-09-05 18:55:47 +1000

    liquors API

--- controller/cocktails.js
+++ controller/cocktails.js
@@ -5,12 +5,28 @@ const router = express.Router();
 
 router.get("/api/cocktails", (req, res) => {});
 
-router.get("/api/liquers", (req, res) => {});
+router.get("/api/liquors", (req, res) => {
+  const sql = "SELECT * FROM liquor";
+  db.query(sql).then((dbRes) => {
+    res.json(dbRes.rows);
+  });
+});
 
 router.get("/api/cabinet", (req, res) => {});
 
 router.post("/api/cabinet", (req, res) => {
-  const {} = req.body;
+  const { user_id, liquor_id, volume } = req.body;
+  const sql = `
+    INSERT INTO cabinet_contents(user_id, liquor_id, volume) 
+    VALUES($1, $2, $3)
+  `;
+  db.query(sql, [user_id, liquor_id, volume])
+    .then((dbRes) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      res.status(500).json({});
+    });
 });
 
 router.delete("/api/cabinet/:id", (req, res) => {
