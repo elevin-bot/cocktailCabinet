922fd41dcdc888bbf3b173f23741e397f32bb061
hidekiprado <104890292+hidekiprado@users.noreply.github.com>
2022-09-08 21:53:23 +1000

    Login Logout Sign-in header

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -1,38 +1,54 @@
 export const renderHeader = () => {
+  const h1Loader = document.createElement("p");
+  h1Loader.textContent = "Loading";
+
   //accessing header nav
   const header = document.querySelector("#header-nav");
+  header.appendChild(h1Loader);
+
   //creating
   const logOUtButton = document.createElement("button");
-  const h1 = document.createElement("h1");
+
   const h1AfterLogin = document.createElement("h1");
   const span = document.createElement("span");
-  //setting
-
-  logOUtButton.textContent = "Log out";
-  h1.textContent = "Cocktail Cabinet";
-  span.textContent = `'s cabinet`;
-
-  logOUtButton.classList.add("hidden");
-  h1AfterLogin.classList.add("hidden");
 
   // checking session , login icon changes if logged in
   axios
     .get("/api/session")
     .then((response) => {
-      console.log(object);
-      // if (response.data.name) {
-      //   h1AfterLogin.textContent = response.data.name;
-      //   logOUtButton.classList.remove("hidden");
-      //   h1AfterLogin.classList.remove("hidden");
-      // }
+      //remove loader
+      header.removeChild(h1Loader);
+      //removing h1 before logged in
+      const h1 = document.querySelector("#h1Header");
+      if (h1) {
+        h1.remove();
+      }
+      //setting
+      h1AfterLogin.textContent = response.data.name;
+      span.textContent = `'s cabinet`;
+      logOUtButton.textContent = "Log out";
+      // appending
+      h1AfterLogin.appendChild(span);
+      header.appendChild(logOUtButton);
+      header.appendChild(h1AfterLogin);
+      renderCabinetView();
     })
     .catch((err) => {
-      alert(err.response.data.message);
+      //remove loader
+      header.removeChild(h1Loader);
+
+      const h1 = document.createElement("h1");
+      h1.id = "h1Header";
+
+      //setting
+      h1.textContent = "Cocktail Cabinet";
+      //appending
+      header.appendChild(h1);
+      renderSignup();
     });
 
-  // appending
-  h1AfterLogin.appendChild(span);
-  header.appendChild(logOUtButton);
-  header.appendChild(h1);
-  header.appendChild(h1AfterLogin);
+  //Event Listeners
+  logOUtButton.addEventListener("click", () => {
+    renderLogOut();
+  });
 };
--- client/js/components/signup-loader.js
+++ client/js/components/signup-loader.js
@@ -1,31 +1,45 @@
 // welcome to your cabinet
-export const renderSignupLoader = () => {
-    // accessing page
-    const page = document.querySelector("#page");
-  
-    // create components
-    const mainComponent = document.createElement("div");
-    const signupLoaderTitle = document.createElement("h1");
-    const signupLoaderButton = document.createElement("button");
-    
-    // setting values
-    signupLoaderTitle.id = "signupLoaderTitle";
-    signupLoaderTitle.textContent = "Welcome to your secret cabinet"
+export const renderSignupLoader = (data) => {
+  delete data.name;
+  // accessing page
+  const page = document.querySelector("#page");
 
-    signupLoaderButton.id = "signupLoaderButton";
-    signupLoaderButton.textContent = "Take me to my cabinet"
+  // create components
+  const mainComponent = document.createElement("div");
+  const signupLoaderTitle = document.createElement("h1");
+  const signupLoaderButton = document.createElement("button");
 
+  // setting values
+  mainComponent.className = "signUpDivMain";
+  signupLoaderTitle.id = "signupLoaderTitle";
+  signupLoaderTitle.textContent = "Welcome to your secret cabinet";
 
-    // appending
-    mainComponent.appendChild(signupLoaderTitle);
-    mainComponent.appendChild(signupLoaderButton);
+  signupLoaderButton.id = "signupLoaderButton";
 
-    // event listener
-    signupLoaderButton.addEventListener("click", () => {
-        renderCabinetList();
-    });
-    
-    // appending page
-    page.appendChild(mainComponent);
-  };
-  
+  signupLoaderButton.textContent = "Take me to my cabinet";
+
+  // appending
+  mainComponent.appendChild(signupLoaderTitle);
+  mainComponent.appendChild(signupLoaderButton);
+
+  // event listener
+  signupLoaderButton.addEventListener("click", () => {
+    //API access
+    axios
+      .post("/api/session", data)
+      .then((response) => {
+        renderCabinetView();
+        renderHeader();
+      })
+      .catch((error) => {
+        if (error.response.status === 500) {
+          alert("Oops, failed to sign up. Please try again.");
+        } else {
+          alert(error.response.data.message);
+        }
+      });
+  });
+
+  // appending page
+  page.replaceChildren(mainComponent);
+};
